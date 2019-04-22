0a46ce817d746d01e70daa22f4a902dd1bc408c6
aquasar <arhoy@ualberta.ca>
2019-04-22 17:51:48 -0600

    keys_prod added contentful env variables

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,8 +2,8 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
     sendGridAPIKEY: process.env.SENDGRID_KEY,
-    // contentful_space: process.env.CONTENTFUL_SPACE,
-    // contentful_accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+    contentful_space: process.env.CONTENTFUL_SPACE,
+    contentful_accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
 
   };
   
