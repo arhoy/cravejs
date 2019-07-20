3ae907a2956397f7b0ec6d4c5323cfa9dff8ea55
aquasar <arhoy@ualberta.ca>
2019-07-20 14:59:25 -0600

    Register Page fix

--- client/src/pages/auth/RegisterPage.js
+++ client/src/pages/auth/RegisterPage.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import Register from '../../components/auth/Register';
 import Layout from '../../components/Layout/Layout';
 import { connect } from 'react-redux';
-import mainLinks from '../../utils/links/mainLinks';
 
 
 class RegisterPage extends Component {
     render() {
-        const { isAuthenticated ,user } = this.props.auth;
-        const links = mainLinks(isAuthenticated, user);
+
         
         return (
-            <Layout links = {links}>
+            <Layout>
                 <Register/>
             </Layout>
         );
