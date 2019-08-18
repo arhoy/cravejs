a52dd1617906646f654da0cfed1e2445559573c4
arhoy <arhoy@ualberta.ca>
2019-08-18 17:14:23 -0600

    add prerender

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,11 +1,10 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY,
-    sendGridAPIKEY: process.env.SENDGRID_KEY,
-    contentful_space: process.env.CONTENTFUL_SPACE_ID,
-    contentful_accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
-    googleAPIKEY: process.env.googleAPIKEY,
-    customerSearchEngineID: process.env.customerSearchEngineID
-
-  };
-  
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  sendGridAPIKEY: process.env.SENDGRID_KEY,
+  contentful_space: process.env.CONTENTFUL_SPACE_ID,
+  contentful_accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+  googleAPIKEY: process.env.googleAPIKEY,
+  customerSearchEngineID: process.env.customerSearchEngineID,
+  prerender_token: process.env.PRERENDER_TOKEN
+};
